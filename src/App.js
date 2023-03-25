a9b94525991d3c7a5ca4109e9aaa2d174dea64e2
jagadish <jagadishbisai02@gmail.com>
2023-03-25 11:53:54 +0000

    different types of condition randering

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import {Component} from 'react'
+
+import Welcome from './components/Welcome'
+
+import './App.css'
+
+class App extends Component {
+  state = {
+    isLoggedIn: false,
+  }
+  // using by if-else statement when user is log then show logOut button other wise logIn button shows
+  /*  renderAuthButton = () => {
+    const {isLoggedIn} = this.state
+    if (isLoggedIn === true) {
+      return <button>Logout</button>
+    }
+    return <button>Login</button>
+  }
+  In div container call {this.renderAuthButton()}
+
+
+   by using Element Variables
+const {isLoggedIn} = this.state
+    let authButton
+    if (isLoggedIn === true) {
+      authButton = <button>Logout</button>
+    } else {
+      authButton = <button>Login</button>
+    }
+
+    In div container call {authButton}
+
+
+    by using ternary operating
+    {isLoggedIn ? <button>Logout</button> : <button>Login</button>}
+    */
+
+  render() {
+    const {isLoggedIn} = this.state
+    return (
+      <div className="container">
+        <Welcome />
+        {isLoggedIn && <button>Logout</button>}
+        {!isLoggedIn && <button>Login</button>}
+      </div>
+    )
+  }
+}
+
+export default App
